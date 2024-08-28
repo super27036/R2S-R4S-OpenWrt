6980e007a071a7389379df02f02f24afbdca6b2a
super27035 <superssatellite@gmail.com>
2024-08-29 00:11:45 +0800

    Create prepare_package_new.sh

---
+++ SCRIPTS/prepare_package_new.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+clear
+
+### 基础部分 ###
+# 使用 O2 级别的优化
+sed -i 's/Os/O2/g' include/target.mk
+# 更新 Feeds
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+### 必要的 Patches ###
+# mount cgroupv2
+# pushd feeds/packages
+# wget -qO - https://github.com/openwrt/packages/commit/7a64a5f4.patch | patch -p1
+# popd
+
+### 获取额外的 LuCI 应用、主题 ###
+# 从 ImmortalWRT 仓库中获取最新的 LuCI 应用和主题
+git clone --depth 1 https://github.com/immortalwrt/luci.git /tmp/luci
+
+# 更新 themes 目录
+cp -rf /tmp/luci/themes/* feeds/luci/themes/
+
+# 更新 applications 目录
+cp -rf /tmp/luci/applications/* feeds/luci/applications/
+
+# 删除临时目录
+rm -rf /tmp/luci
+
+# MOD Argon
+rm -rf feeds/luci/themes/luci-theme-argon
+git clone -b randomPic --depth 1 https://github.com/msylgj/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+
+# DNSPod
+git clone -b main --depth 1 https://github.com/msylgj/luci-app-tencentddns.git feeds/luci/applications/luci-app-tencentddns
+ln -sf ../../../feeds/luci/applications/luci-app-tencentddns ./package/feeds/luci/luci-app-tencentddns
+
+# WeChatPush
+rm -rf feeds/luci/applications/luci-app-wechatpush
+git clone -b master --depth 1 https://github.com/tty228/luci-app-wechatpush.git feeds/luci/applications/luci-app-wechatpush
+
+# geodata
+rm -rf feeds/packages/net/v2ray-geodata
+git clone -b master --depth 1 https://github.com/QiuSimons/openwrt-mos.git ./openwrt-mos
+cp -rf ./openwrt-mos/v2ray-geodata feeds/packages/net/v2ray-geodata & rm -rf ./openwrt-mos
+
+# 更换 Nodejs 版本
+rm -rf ./feeds/packages/lang/node
+git clone https://github.com/sbwml/feeds_packages_lang_node-prebuilt feeds/packages/lang/node
+
+### 最后的收尾工作 ###
+# Lets Fuck
+if [ ! -d "package/base-files/files/usr/bin" ]; then
+    mkdir package/base-files/files/usr/bin
+fi
+cp -f ../SCRIPTS/fuck package/base-files/files/usr/bin/fuck
+
+# 定制化配置
+sed -i "s/'%D %V %C'/'Built by OPoA($(date +%Y.%m.%d))@%D %V'/g" package/base-files/files/etc/openwrt_release
+sed -i "/DISTRIB_REVISION/d" package/base-files/files/etc/openwrt_release
+sed -i "/%D/a\ Built by OPoA($(date +%Y.%m.%d))" package/base-files/files/etc/banner
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+
+# Modify default hostname
+sed -i 's/ImmortalWrt/SUPERouter/g' package/base-files/files/bin/config_generate
+
+# Password
+# sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/$1$S2TRFyMU$E8fE0RRKR0jNadn3YLrSQ0:18690:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
+
+sed -i 's/1608/1800/g' package/emortal/cpufreq/files/cpufreq.uci
+sed -i 's/2016/2208/g' package/emortal/cpufreq/files/cpufreq.uci
+sed -i 's/1512/1608/g' package/emortal/cpufreq/files/cpufreq.uci
+
+# 生成默认配置及缓存
+rm -rf .config
+
+# 清理可能因patch存在的冲突文件
+find ./ -name *.orig | xargs rm -rf
+find ./ -name *.rej | xargs rm -rf
+
+exit 0

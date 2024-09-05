174048bbf0165790c056d7fd512bbb604b82c9db
super27035 <superssatellite@gmail.com>
2024-09-05 22:59:43 +0800

    Update prepare_package.sh

--- SCRIPTS/prepare_package.sh
+++ SCRIPTS/prepare_package.sh
@@ -1,11 +1,14 @@
-# 注：这个.sh脚本可以成功编译出lede库中的frp，能把自己的frp预设编译进去 2014-08-29
-
 #!/bin/bash
 clear
 
 ### 基础部分 ###
 # 使用 O2 级别的优化
 sed -i 's/Os/O2/g' include/target.mk
+
+### 集成 iStore ###
+# 在 feeds.conf.default 中添加 iStore 源
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+
 # 更新 Feeds
 ./scripts/feeds update -a
 ./scripts/feeds install -a
@@ -31,7 +34,6 @@ ln -sf ../../../feeds/luci/applications/luci-app-tencentddns ./package/feeds/luc
 # git clone https://github.com/jerrykuku/lua-maxminddb.git feeds/luci/applications/lua-maxminddb
 # 完整克隆 luci-app-vssr 仓库
 # git clone https://github.com/super27035/luci-app-vssr.git feeds/luci/applications/luci-app-vssr
-# git clone https://github.com/MilesPoupart/luci-app-vssr.git feeds/luci/applications/luci-app-vssr
 # -------------- #
 
 # WeChatPush
@@ -57,15 +59,8 @@ git clone --depth 1 https://github.com/coolsnowwolf/luci.git /tmp/luci
 cp -rf /tmp/luci/applications/luci-app-frpc feeds/luci/applications/
 cp -rf /tmp/luci/applications/luci-app-frps feeds/luci/applications/
 
-# 从 super27035 仓库中获取 luci-app-ssr-plus 应用
-git clone --depth 1 https://github.com/super27035/small-package.git /tmp/small-package
-
-# 复制 luci-app-ssr-plus 应用到 feeds/luci/applications/
-cp -rf /tmp/small-package/luci-app-ssr-plus feeds/luci/applications/
-
 # 删除临时目录
 rm -rf /tmp/luci
-rm -rf /tmp/small-package
 # -------------- #
 
 ### 最后的收尾工作 ###
@@ -80,7 +75,10 @@ sed -i "/DISTRIB_REVISION/d" package/base-files/files/etc/openwrt_release
 sed -i "/%D/a\ Built by SUPER($(date +%Y.%m.%d))" package/base-files/files/etc/banner
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
-# Modify default hosename
+# Disable ipv6
+sed -i 's/def_bool y/def_bool n/g' config/Config-build.in
+
+# Modify default hostname
 sed -i 's/ImmortalWrt/SUPERouter/g' package/base-files/files/bin/config_generate
 # Password
 # sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/$1$S2TRFyMU$E8fE0RRKR0jNadn3YLrSQ0:18690:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings

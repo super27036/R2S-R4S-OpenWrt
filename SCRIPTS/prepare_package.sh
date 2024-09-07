c1b61961e74c764ee4745b036ffe9ada523dd673
super27035 <superssatellite@gmail.com>
2024-09-07 12:12:20 +0800

    Update prepare_package.sh

--- SCRIPTS/prepare_package.sh
+++ SCRIPTS/prepare_package.sh
@@ -7,10 +7,10 @@ sed -i 's/Os/O2/g' include/target.mk
 
 ### 集成 iStore ###
 # 在 feeds.conf.default 中添加 iStore 源
-echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+# echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 
 # 添加 feeds.conf.default 中添加 poweroff 源
-echo 'src-git poweroff https://github.com/super27035/luci-app-poweroff.git;master' >> feeds.conf.default
+# echo 'src-git poweroff https://github.com/super27035/luci-app-poweroff.git;master' >> feeds.conf.default
 
 
 # 更新 Feeds
